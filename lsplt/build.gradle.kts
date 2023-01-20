204a1636884d397617101e2bae3a78dd3febb163
vvb2060 <vvb2060@gmail.com>
2023-01-21 05:24:02 +0800

    Update gradle

--- lsplt/build.gradle.kts
+++ lsplt/build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.eclipse.jgit:org.eclipse.jgit:6.3.0.202209071007-r")
+        classpath("org.eclipse.jgit:org.eclipse.jgit:6.4.0.202211300538-r")
     }
 }
 
